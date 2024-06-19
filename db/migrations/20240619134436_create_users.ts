409a5b1c4a964a1f33d08c8cafcd4acdcd5ce27d
henriquesbarros <henriquesbarroshs@gmail.com>
2024-06-19 11:10:59 -0300

    feat(db): create new migration for adding users table

---
+++ db/migrations/20240619134436_create_users.ts
@@ -0,0 +1,15 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.string('name', 100).notNullable()
+    table.string('email', 100).notNullable().unique()
+    table.string('password', 255).notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
