1423c1ed7a88ba4050958e3cf400f1ea0e21fdf1
henriquesbarros <henriquesbarroshs@gmail.com>
2024-06-20 01:00:11 -0300

    feat: login route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from 'node:crypto'
 import fastify from 'fastify'
+import jwt from 'jsonwebtoken'
 import fastifyJwt from '@fastify/jwt'
 import fastifyCookie from '@fastify/cookie'
 import bcrypt from 'bcrypt'
@@ -9,6 +10,18 @@ import { knex } from './database'
 import { env } from './env'
 
 const app = fastify()
+
+const generateTokens = (userId: string) => {
+  const accessToken = jwt.sign({ userId }, env.JWT_SECRET, {
+    expiresIn: env.ACCESS_TOKEN_EXPIRY,
+  })
+
+  const refreshToken = jwt.sign({ userId }, env.REFRESH_SECRET, {
+    expiresIn: env.REFRESH_TOKEN_EXPIRY,
+  })
+
+  return { accessToken, refreshToken }
+}
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
@@ -47,6 +60,44 @@ app.post('/register', async (request, reply) => {
     reply.status(201).send()
   } catch {
     reply.status(500).send({ error: 'Error creating user' })
+  }
+})
+
+app.post('/login', async (request, reply) => {
+  const loginBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { email, password } = loginBodySchema.parse(request.body)
+
+  try {
+    const user = await knex('users').where({ email }).first()
+
+    const isMatchingPassword = await bcrypt.compare(password, user.password)
+
+    if (user && isMatchingPassword) {
+      const { accessToken, refreshToken } = generateTokens(user.id)
+
+      const now = new Date()
+      const accessExpiresAt = new Date(now.getTime() + 15 * 60 * 1000) // 15 min
+      const refreshExpiresAt = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000) // 7 days
+
+      await knex('sessions').insert({
+        id: randomUUID(),
+        user_id: user.id,
+        token: accessToken,
+        refresh_token: refreshToken,
+        expires_at: accessExpiresAt,
+        refresh_expires_at: refreshExpiresAt,
+      })
+
+      reply.status(200).send({ accessToken, refreshToken })
+    } else {
+      reply.status(401).send({ error: 'Invalid email or password!' })
+    }
+  } catch (error) {
+    reply.status(500).send({ error: 'Error logging in!' })
   }
 })
 
