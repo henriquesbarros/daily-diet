1a7ac152119376d0e117b62a6c89ae758fcb7a6a
henriquesbarros <henriquesbarroshs@gmail.com>
2024-06-20 00:40:56 -0300

    feat: user creation route

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,6 +6,10 @@ const envSchema = z.object({
   DATABASE_URL: z.string(),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   PORT: z.number().default(3333),
+  JWT_SECRET: z.string(),
+  REFRESH_SECRET: z.string(),
+  ACCESS_TOKEN_EXPIRY: z.string().default('15m'),
+  REFRESH_TOKEN_EXPIRY: z.string().default('7d'),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,53 @@
+import { randomUUID } from 'node:crypto'
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+import bcrypt from 'bcrypt'
+import { z } from 'zod'
+
 import { knex } from './database'
 import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: { expiresIn: env.ACCESS_TOKEN_EXPIRY },
+})
 
-  return tables
+app.register(fastifyCookie)
+
+app.decorate('authenticate', async (request, reply) => {
+  try {
+    await request.jwtVerify()
+  } catch (err) {
+    reply.send(err)
+  }
+})
+
+app.post('/register', async (request, reply) => {
+  const createUserBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string(),
+  })
+
+  const { name, email, password } = createUserBodySchema.parse(request.body)
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  try {
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      password: hashedPassword,
+    })
+
+    reply.status(201).send()
+  } catch {
+    reply.status(500).send({ error: 'Error creating user' })
+  }
 })
 
 app
