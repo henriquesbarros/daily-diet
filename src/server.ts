0c94373082f3a4f167f28142a5b411c5c6d8858b
henriquesbarros <henriquesbarroshs@gmail.com>
2024-06-20 11:00:30 -0300

    feat: refresh token route

--- src/server.ts
+++ src/server.ts
@@ -101,6 +101,63 @@ app.post('/login', async (request, reply) => {
   }
 })
 
+app.post('/token', async (request, reply) => {
+  const { refreshToken } = request.body as { refreshToken: string }
+
+  if (!refreshToken) {
+    return reply.status(401).send({ error: 'Refresh token is required' })
+  }
+
+  try {
+    const session = await knex('sessions')
+      .where({ refresh_token: refreshToken })
+      .first()
+
+    if (!session) {
+      return reply.status(403).send({ error: 'Invalid refresh token' })
+    }
+
+    if (new Date(session.refresh_expires_at) < new Date()) {
+      return reply.status(403).send({ error: 'Refresh token expired' })
+    }
+
+    jwt.verify(refreshToken, env.REFRESH_SECRET, async (err, decoded) => {
+      if (err) {
+        return reply.status(403).send({ error: 'Invalid refresh token' })
+      }
+
+      if (
+        typeof decoded === 'object' &&
+        decoded !== null &&
+        'userId' in decoded
+      ) {
+        const userId = (decoded as { userId: string }).userId
+        const { accessToken, refreshToken: newRefreshToken } =
+          generateTokens(userId)
+
+        const now = new Date()
+        const accessExpiresAt = new Date(now.getTime() + 15 * 60 * 1000) // 15 min
+        const refreshExpiresAt = new Date(
+          now.getTime() + 7 * 24 * 60 * 60 * 1000,
+        ) // 7 days
+
+        await knex('sessions').where({ id: session.id }).update({
+          token: accessToken,
+          refresh_token: newRefreshToken,
+          expires_at: accessExpiresAt,
+          refresh_expires_at: refreshExpiresAt,
+        })
+
+        reply.status(200).send({ accessToken, refreshToken: newRefreshToken })
+      } else {
+        reply.status(403).send({ error: 'Invalid token payload' })
+      }
+    })
+  } catch (error) {
+    reply.status(500).send({ error: 'Error refreshing token' })
+  }
+})
+
 app
   .listen({
     port: env.PORT,
